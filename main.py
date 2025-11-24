4a106614aaf36fbcc9d70752e01bdfa1867216ca
RosieAHill <Roseaahill@gmail.com>
2025-11-24 10:30:02 -0500

    refactor to include display functions

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import parse
 from gen import my_config, my_json
+from display import game_map
 
 config = parse.config("config.ini")
 
-def run_if_yes(callback, settings, prompt) -> str:
+map_path_to_display = "input/my_mountain_map.json"
+
+def gen_if_yes(callback, settings, prompt) -> str:
     user_answer = input(prompt)
     response = "Skipping..."
     if user_answer.lower() == "y":
@@ -13,8 +16,13 @@ def run_if_yes(callback, settings, prompt) -> str:
 def main():
     print(f"Settings are{" " if len(config.sections()) > 0 else " not "}loaded...")
 
-    print(f"{run_if_yes(my_config, config["IO_DATA"], "Generate config file? (y/N) ")}")
-    print(f"{run_if_yes(my_json, config["IO_DATA"], "Generate json file? (y/N) ")}")
+    print(f"{gen_if_yes(my_config, config["IO_DATA"], "Generate config file? (y/N) ")}")
+    print(f"{gen_if_yes(my_json, config["IO_DATA"], "Generate json file? (y/N) ")}")
+    print(f"{gen_if_yes(game_map, config["MAP_FEATURES"], "Generate map data? (y/N) ")}")
+    
+    map_data: dict = parse.json_data(map_path_to_display)
+    
+    print(f"Displaying map from '{map_path_to_display}'...\n{game_map(map_data["data"],config["MAP_FEATURES"])}")
 
 if __name__ == "__main__":
     main()
